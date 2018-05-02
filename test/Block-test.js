7e27bf3d1d56dd14839ef28087b2f4c3dfbea759
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-05-02 15:24:45 -0600

    Pass second block test

--- test/Block-test.js
+++ test/Block-test.js
@@ -6,10 +6,19 @@ describe ('Block', function() {
 	it('should return true', function() {
 		assert.equal(true, true);
 	})
+
+	it('should have a default height and width', function() {
+		var block = new Block();
+		
+		assert.equal(block.height, 13);
+		assert.equal(block.width, 13);
+	})
+
+	it('should take x and y as arguments', function() {
+
+	})
+
 })
-
-describe ('')
-
 //Setup
 
 //Execution
