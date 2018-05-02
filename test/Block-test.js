389b558bd8354f0edaf693480b0921c59c8c1ab9
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-05-02 17:02:12 -0600

    Pass Block test 3 and 4 and begin refactor block constructor

--- test/Block-test.js
+++ test/Block-test.js
@@ -1,22 +1,37 @@
 const chai = require ('chai');
 const assert = chai.assert;
 const Block = require('../lib/Block.js');
+require('locus');
 
 describe ('Block', function() {
 	it('should return true', function() {
 		assert.equal(true, true);
 	})
 
+	it('should take x and y as arguments', function() {
+		var block = new Block(15, 15);
+		assert.equal(block.x, 15);
+		assert.equal(block.y, 15);
+	})
+
 	it('should have a default height and width', function() {
 		var block = new Block();
-		
 		assert.equal(block.height, 13);
 		assert.equal(block.width, 13);
 	})
 
-	it('should take x and y as arguments', function() {
-
+	it('should have default x and y velocities', function() {
+		var block = new Block();
+		assert.equal(block.dx, 14);
+		assert.equal(block.dy, 4);
 	})
+
+	// it('should be able to draw itself again', function() {
+	// 	window.innerwidth = 400;
+	// 	var block = new Block();
+	// 	assert.equal(block.draw(canvas.x), window.innerwidth/2)
+	// })
+	
 
 })
 //Setup
