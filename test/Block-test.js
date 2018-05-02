09bea452b16ef498f88b137629e847787ae47264
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-05-02 15:11:04 -0600

    Pass first Block test

---
+++ test/Block-test.js
@@ -0,0 +1,17 @@
+const chai = require ('chai');
+const assert = chai.assert;
+const Block = require('../lib/Block.js');
+
+describe ('Block', function() {
+	it('should return true', function() {
+		assert.equal(true, true);
+	})
+})
+
+describe ('')
+
+//Setup
+
+//Execution
+
+//Assertion
