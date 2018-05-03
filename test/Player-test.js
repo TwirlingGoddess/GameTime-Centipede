b46e227260b4cfb6c224813441b6064f3974fb5a
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-05-03 09:06:25 -0600

    Create player constructor

---
+++ test/Player-test.js
@@ -0,0 +1,27 @@
+const chai = require ('chai');
+const assert = chai.assert;
+const Block = require('../lib/Player.js');
+require('locus');
+
+describe ('Player', function() {
+	it.only('should return true', function() {
+		assert.equal(true, true);
+	})
+
+	// it('should take x and y as arguments', function() {
+	// 	var player = new Plock(15, 15);
+	// 	assert.equal(player.x, 15);
+	// 	assert.equal(player.y, 15);
+	// })
+
+	// it('should have a default height and width', function() {
+	// 	var player = new Player();
+	// 	assert.equal(player.height, 13);
+	// 	assert.equal(player.width, 13);
+	// })
+
+	// it('should have default x and y velocities', function() {
+	// 	var block = new Player();
+	// 	assert.equal(player.dx, 14);
+	// 	assert.equal(player.dy, 4);
+	// })
